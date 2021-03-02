c781c85fc497e4dc90901a48c1c9723c108418e4
Thanos Kandylas <55313245+ThanosKand@users.noreply.github.com>
2021-03-02 18:56:24 +0100

    Create calclight.m

---
+++ calclight.m
@@ -0,0 +1,25 @@
+function I = calclight(z,t,C,dx,kp,kw,I0)
+int=zeros;
+
+for i = 2:length(z)
+    int(i) = int(i-1) + kp*C(i)*dx;
+   
+end
+
+% g1 = (1-0.8.*sin((pi.*lat)/180).*cos(2.*pi.*(t./365)));
+g1 = 1+cos(2.*pi.*(t./365));
+I0 = I0 * g1;
+
+I = I0.*(exp(-kw*z) .* exp(-int));
+I(1) = I0.*(exp(-kw.*z(1)) .* exp(-kp.*C(1).*dx));
+
+end
+
+
+
+% zzz = 1:50;
+% 
+% cc = 1000 * (exp(-2) * exp(-0.2 * zzz));
+% % cc = 1000 * (exp(-0.2 * zzz))
+% figure
+% plot(zzz, cc)
